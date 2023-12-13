2ef1ce3dea98e706f5bc19570f2069756992cf40
Hannah <30120etta@gmail.com>
2023-12-13 10:01:22 +0800

    coupon-edit更新

--- coupon-edit.php
+++ coupon-edit.php
@@ -27,7 +27,14 @@ $row = $result->fetch_assoc();
 
     <?php
     include("../ysl_project/ysl-css.php");
+    include("../ysl_project/ysl-nav.php");
+    //include("../ysl_project/sellerDashboard_sideNav.php");
     ?>
+    <style>
+    .input-error {
+      color: red;
+    }
+  </style>
 </head>
 
 <body>
@@ -56,7 +63,7 @@ $row = $result->fetch_assoc();
         <?php if ($couponCount == 0) : ?>
             <h1>此優惠券尚未建立</h1>
         <?php else : ?>
-            <form action="doUpdateCoupon.php" method="post">
+            <form action="doUpdateCoupon.php" method="post" id="updateCouponform" onsubmit="return validateForm()">
                 <table class="table table-bordered">
                     <input type="hidden" name="coupon_id" value="<?= $row["coupon_id"] ?>">
                     <tr>
@@ -86,32 +93,35 @@ $row = $result->fetch_assoc();
                     <tr>
                         <th>折扣百分比或折扣金額</th>
                         <td>
-                            <input type="text" class="form-control" name="discount_value" value="<?= $row["discount_value"] ?>">
+                            <input type="text" class="form-control" name="discount_value" id="discount_value" value="<?= $row["discount_value"] ?>">
+                            <span id="discountValueError" class="input-error"></span>
                         </td>
                     </tr>
                     <tr>
                         <th>可使用次數</th>
                         <td>
-                            <input type="text" class="form-control" name="usage_times" value="<?= $row["usage_times"] ?>">
+                            <input type="text" class="form-control" name="usage_times" id="usage_times" value="<?= $row["usage_times"] ?>" oninput="validateNumberInput(this)">
+                            <span id="usage_timesError" class="input-error"></span>
                         </td>
                     </tr>
                     <tr>
                         <th>起始日</th>
                         <td>
-                            <input type="date" class="form-control" name="start_date" value="<?= $row["start_date"] ?>">
+                            <input type="date" class="form-control" name="start_date" id="start_date" value="<?= $row["start_date"] ?>">
                         </td>
                     </tr>
                     <tr>
                         <th>截止日</th>
                         <td>
-                            <input type="date" class="form-control" name="expiration_date" value="<?= $row["expiration_date"] ?>">
+                            <input type="date" class="form-control" name="expiration_date" id="expiration_date" value="<?= $row["expiration_date"] ?>">
+                            <span id="dateError" class="input-error pt-1"></span>
                         </td>
                     </tr>
                     <tr>
                         <th>使用門檻(滿多少錢可使用)</th>
                         <td>
-                            <input type="text" class="form-control" name="price_rule" value="<?= $row["price_rule"] ?>">
-
+                            <input type="text" class="form-control" name="price_rule" id="price_rule" value="<?= $row["price_rule"] ?>" oninput="validateNumberInput(this)">
+                            <span id="price_ruleError" class="input-error"></span>
                         </td>
                     </tr>
                     <tr>
@@ -176,6 +186,85 @@ $row = $result->fetch_assoc();
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <!-- 驗證截止日期不會早於開始日期 -->
+    <script>
+        function validateDate() {
+            //獲得開始日期和截止日期的值
+            let startDate = document.getElementById('start_date').value;
+            let expirationDate = document.getElementById('expiration_date').value;
+            //把日期字串轉換成date
+            let startDateObj = new Date(startDate);
+            let expirationDateObj = new Date(expirationDate);
+            //清空日期錯誤訊息
+            document.getElementById('dateError').innerHTML = '';
+            //驗證截止日期是否早於開始日期
+            if (expirationDateObj < startDateObj) {
+                //顯示錯誤
+                document.getElementById('dateError').innerHTML = '優惠券截止日期不可以早於開始日期';
+                return false;
+            } else {
+                return true;
+            }
+        }
+    </script>
+    <!-- 驗證input -->
+    <script>
+        function validateNumberInput(inputElement) {
+            let inputValue = inputElement.value.trim();
+            let errorElementId = inputElement.id + 'Error';
+            //清空錯誤訊息
+            document.getElementById(errorElementId).innerHTML = '';
+
+            //驗證是否為數字
+            if (!/^\d+$/.test(inputValue)) {
+                document.getElementById(errorElementId).innerHTML = '請輸入數字';
+            }
+        }
+
+        function clearErrors() {
+            //清空所有錯誤訊息
+            let errorElements = document.getElementsByClassName('input-error');
+            for (let i = 0; i < errorElements.length; i++) {
+                errorElements[i].innerHTML = '';
+            }
+        }
+
+        function validateDiscountValue() {
+            let discountType = document.querySelector('input[name="discount_type"]:checked').value;
+            let discountValueInput = document.getElementById('discount_value');
+            let discountValue = discountValueInput.value.trim();
+            //驗證discount_value是否為數字
+            if (!/^\d+$/.test(discountValue)) {
+                document.getElementById('discountValueError').innerHTML = '請輸入數字';
+                return false;
+            }
+            //優惠券類型為百分比時，驗證discount_value
+            if (discountType === 'percentage') {
+                discountValue = parseInt(discountValue);
+                //驗證discount_value範圍        
+                if (isNaN(discountValue) || discountValue < 0 || discountValue > 100) {
+                    document.getElementById('discountValueError').innerHTML = '請輸入有效的百分比（0-100）';
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        function validateForm() {
+            clearErrors();
+            //檢查可使用次數是否為數字
+            validateNumberInput(document.getElementById('usage_times'));
+            //檢查最低消費金額是否為數字
+            validateNumberInput(document.getElementById('price_rule'));
+            //日期驗證
+            let isDateValid = validateDate();
+            if (!validateDiscountValue()) {
+                return false;
+            }
+            //全部都通過：返回true
+            return isDateValid;
+        }
+    </script>
 </body>
 
 </html>
