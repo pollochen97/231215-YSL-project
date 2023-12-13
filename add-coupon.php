ad4edfba8bdf3e806bbb9038aa526bbba3dd8a96
Hannah <30120etta@gmail.com>
2023-12-13 18:07:30 +0800

    更新add-coupon type option

--- add-coupon.php
+++ add-coupon.php
@@ -9,8 +9,7 @@
 
   <?php
   include("../ysl_project/ysl-css.php");
-  include("../ysl_project/ysl-nav.php");
-  //include("../ysl_project/sellerDashboard_sideNav.php");
+  include("../231215-YSL-project/admin-nav.php");
   ?>
   <style>
     .input-error {
@@ -20,117 +19,123 @@
 
 </head>
 
-<body>
-  <div class="container">
-    <!-- Back to coupon list -->
-    <div class="py-2">
-      <a href="coupon-list.php" class="btn btn-warning" title="回到優惠券一覽列表"><i class="fa-solid fa-arrow-left"></i></a>
-    </div>
-    <!-- Add Coupon form start -->
-    <form action="doAddCoupon.php" method="post" id="addCouponform" onsubmit="return validateForm()">
-      <div class="mb-3">
-        <label for="title" class="form-label">優惠券名稱：</label>
-        <input type="text" class="form-control validate-input" id="title" placeholder="請輸入優惠券名稱（限20字元內）" name="title" required>
-        <span id="titleError" class="input-error"></span>
-      </div>
-      <div class="mb-3">
-        <label for="coupon_code" class="form-label">優惠券代碼：</label>
-        <input type="text" class="form-control validate-input" id="randomCouponInput" placeholder="請輸入優惠券代碼，限20字元內的數字、英文大小寫" name="coupon_code" required>
-        <span id="randomCouponInputError" class="input-error pt-1"></span>
-        <h6 class="small text-secondary p-2">(可自定義20字元內的大小寫英文、數字混雜字元)</h6>
-        <button class="btn btn-warning mb-3" onclick="generateRandomCouponCode()">隨機生成一組代碼</button>
-        <div class="mb-3 row">
-          <div class="col-6">
-            <label for="discount_type" class="form-label">優惠券類型：</label>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="discount_type" id="percentage" value="percentage" required>
-              <label class="form-check-label" for="percentage">
-                依售價百分比折價
-              </label>
+<body class="sb-nav-fixed">
+  <div id="layoutSidenav">
+    <div id="layoutSidenav_content">
+    <?php include("../231215-YSL-project/admin_dashboard.php"); ?>
+    <main>
+      <div class="container">
+        <!-- Back to coupon list -->
+        <div class="py-2">
+          <a href="coupon-list.php" class="btn btn-warning" title="回到優惠券一覽列表"><i class="fa-solid fa-arrow-left"></i></a>
+        </div>
+        <!-- Add Coupon form start -->
+        <form action="doAddCoupon.php" method="post" id="addCouponform" onsubmit="return validateForm()">
+          <div class="mb-3">
+            <label for="title" class="form-label">優惠券名稱：</label>
+            <input type="text" class="form-control validate-input" id="title" placeholder="請輸入優惠券名稱（限20字元內）" name="title" required>
+            <span id="titleError" class="input-error"></span>
+          </div>
+          <div class="mb-3">
+            <label for="coupon_code" class="form-label">優惠券代碼：</label>
+            <input type="text" class="form-control validate-input" id="randomCouponInput" placeholder="請輸入優惠券代碼，限20字元內的數字、英文大小寫" name="coupon_code" required>
+            <span id="randomCouponInputError" class="input-error pt-1"></span>
+            <h6 class="small text-secondary p-2">(可自定義20字元內的大小寫英文、數字混雜字元)</h6>
+            <button class="btn btn-warning mb-3" onclick="generateRandomCouponCode()">隨機生成一組代碼</button>
+            <div class="mb-3 row">
+              <div class="col-6">
+                <label for="discount_type" class="form-label">優惠券類型：</label>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="discount_type" id="percentage" value="percentage" required>
+                  <label class="form-check-label" for="percentage">
+                    依售價百分比折價
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="discount_type" id="amount" value="amount" required>
+                  <label class="form-check-label" for="amount">
+                    依優惠金額折價
+                  </label>
+                </div>
+              </div>
+              <div class="col-6">
+                <label for="discount_value" class="form-label">優惠券折扣百分比／折扣金額：</label>
+                <input type="text" class="form-control" id="discount_value" placeholder="請輸入折扣的百分比或是折扣金額" name="discount_value" required>
+                <span id="discountValueError" class="input-error"></span>
+              </div>
             </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="discount_type" id="amount" value="amount" required>
-              <label class="form-check-label" for="amount">
-                依優惠金額折價
-              </label>
+            <div class="mb-3">
+              <label for="usage_times" class="form-label">可使用次數：</label>
+              <input type="text" class="form-control" id="usage_times" placeholder="請輸入可使用次數" name="usage_times" oninput="validateNumberInput(this)" required>
+              <span id="usage_timesError" class="input-error"></span>
             </div>
+            <div class="mb-3 row">
+              <div class="col-6">
+                <label for="start_date" class="form-label">優惠券開始日期：</label>
+                <input type="date" class="form-control" id="start_date" name="start_date" required>
+              </div>
+              <div class="col-6">
+                <label for="expiration_date" class="form-label">優惠券截止日期：</label>
+                <input type="date" class="form-control" id="expiration_date" name="expiration_date" required>
+              </div>
+              <span id="dateError" class="input-error pt-1"></span>
+            </div>
+            <div class="mb-3">
+              <label for="price_rule" class="form-label">最低消費金額：</label>
+              <input type="text" class="form-control" id="price_rule" placeholder="請輸入最低消費金額" name="price_rule" oninput="validateNumberInput(this)" required>
+              <span id="price_ruleError" class="input-error"></span>
+            </div>
+            <div class="mb-3">
+              <label for="applicable_scope" class="form-label">優惠券使用範圍</label>
+              <select class="form-select" aria-label="applicable_scope" name="applicable_scope" id="applicable_scope" onchange="updateApplicableTypeOptions()" required>
+                <option>請選擇優惠券使用範圍</option>
+                <option value="global">全站</option>
+                <option value="type">依類別</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="applicable_type_id" class="form-label">優惠券使用類別</label>
+              <select class="form-select" aria-label="applicable_type_id" name="applicable_type_id">
+                <option selected>請選擇優惠券使用類別</option>
+                <option value="1">RPG</option>
+                <option value="2">AVG</option>
+                <option value="3">ETC</option>
+                <option value="4">ACT</option>
+                <option value="5">SLG</option>
+                <option value="6">ARPG</option>
+                <option value="7">SRPG</option>
+                <option value="8">RAC</option>
+                <option value="9">SPG</option>
+                <option value="10">STG</option>
+                <option value="11">AAPG</option>
+                <option value="12">FTG</option>
+                <option value="0" id="global_option">全站使用</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="status" class="form-label">優惠券狀態：</label>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="status" id="status_1" value="1" required>
+                <label class="form-check-label" for="status_1">
+                  可使用
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="status" id="status_0" value="0" required>
+                <label class="form-check-label" for="status_0">
+                  未啟用
+                </label>
+              </div>
+            </div>
+            <?php //endif; 
+            ?>
+            <button class="btn btn-warning mb-3" type="submit">建立優惠券</button>
           </div>
-          <div class="col-6">
-            <label for="discount_value" class="form-label">優惠券折扣百分比／折扣金額：</label>
-            <input type="text" class="form-control" id="discount_value" placeholder="請輸入折扣的百分比或是折扣金額" name="discount_value" required>
-            <span id="discountValueError" class="input-error"></span>
-          </div>
-        </div>
-        <div class="mb-3">
-          <label for="usage_times" class="form-label">可使用次數：</label>
-          <input type="text" class="form-control" id="usage_times" placeholder="請輸入可使用次數" name="usage_times" oninput="validateNumberInput(this)" required>
-          <span id="usage_timesError" class="input-error"></span>
-        </div>
-        <div class="mb-3 row">
-          <div class="col-6">
-            <label for="start_date" class="form-label">優惠券開始日期：</label>
-            <input type="date" class="form-control" id="start_date" name="start_date" required>
-          </div>
-          <div class="col-6">
-            <label for="expiration_date" class="form-label">優惠券截止日期：</label>
-            <input type="date" class="form-control" id="expiration_date" name="expiration_date" required>
-          </div>
-          <span id="dateError" class="input-error pt-1"></span>
-        </div>
-        <div class="mb-3">
-          <label for="price_rule" class="form-label">最低消費金額：</label>
-          <input type="text" class="form-control" id="price_rule" placeholder="請輸入最低消費金額" name="price_rule" oninput="validateNumberInput(this)" required>
-          <span id="price_ruleError" class="input-error"></span>
-        </div>
-        <div class="mb-3">
-          <label for="applicable_scope" class="form-label">優惠券使用範圍</label>
-          <select class="form-select" aria-label="applicable_scope" name="applicable_scope" id="applicable_scope" onchange="updateApplicableTypeOptions()" required>
-            <option>請選擇優惠券使用範圍</option>
-            <option value="global">全站</option>
-            <option value="type">依類別</option>
-          </select>
-        </div>        
-        <div class="mb-3">
-          <label for="applicable_type_id" class="form-label">優惠券使用類別</label>
-          <select class="form-select" aria-label="applicable_type_id" name="applicable_type_id">
-            <option selected>請選擇優惠券使用類別</option>            
-            <option value="1">RPG</option>
-            <option value="2">AVG</option>
-            <option value="3">ETC</option>
-            <option value="4">ACT</option>
-            <option value="5">SLG</option>
-            <option value="6">ARPG</option>
-            <option value="7">SRPG</option>
-            <option value="8">RAC</option>
-            <option value="9">SPG</option>
-            <option value="10">STG</option>
-            <option value="11">AAPG</option>
-            <option value="12">FTG</option>
-            <option value="0">全站使用</option>
-          </select>
-        </div>
-        <div class="mb-3">
-          <label for="status" class="form-label">優惠券狀態：</label>
-          <div class="form-check">
-            <input class="form-check-input" type="radio" name="status" id="status_1" value="1" required>
-            <label class="form-check-label" for="status_1">
-              可使用
-            </label>
-          </div>
-          <div class="form-check">
-            <input class="form-check-input" type="radio" name="status" id="status_0" value="0" required>
-            <label class="form-check-label" for="status_0">
-              未啟用
-            </label>
-          </div>
-        </div>
-        <?php //endif; 
-        ?>
-        <button class="btn btn-warning mb-3" type="submit">建立優惠券</button>
-      </div>
-    </form>
-
-  </div>
+        </form>
+       </main>
+      </div><!--layoutSidenav_content"-->
+    </div><!--layoutSidenav-->
+  </div><!-- container -->
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
   </script>
@@ -179,17 +184,18 @@
   </script>
   <!-- 驗證input -->
   <script>
-    function validateNumberInput(inputElement){
+    function validateNumberInput(inputElement) {
       let inputValue = inputElement.value.trim();
       let errorElementId = inputElement.id + 'Error';
       //清空錯誤訊息
-      document.getElementById(errorElementId).innerHTML='';
+      document.getElementById(errorElementId).innerHTML = '';
 
       //驗證是否為數字
-      if(!/^\d+$/.test(inputValue)){
-        document.getElementById(errorElementId).innerHTML='請輸入數字';
-      }      
+      if (!/^\d+$/.test(inputValue)) {
+        document.getElementById(errorElementId).innerHTML = '請輸入數字';
+      }
     }
+
     function clearErrors() {
       //清空所有錯誤訊息
       let errorElements = document.getElementsByClassName('input-error');
@@ -218,6 +224,7 @@
       }
       return true;
     }
+
     function validateForm() {
       clearErrors();
       //檢查可使用次數是否為數字
@@ -243,8 +250,8 @@
           return false;
         }
       }
-      
-      if(!validateDiscountValue()){
+
+      if (!validateDiscountValue()) {
         return false;
       }
       //全部都通過：返回true
@@ -253,18 +260,21 @@
   </script>
   <!-- 增加type option的判定 -->
   <script>
-    function updateApplicableTypeOptions(){
+    function updateApplicableTypeOptions() {
       let scopeSelect = document.getElementById('applicable_scope');
       let typeSelect = document.getElementsByName('applicable_type_id')[0];
+      let globalOption = document.getElementById('global_option');
 
-      if (scopeSelect.value === 'global'){
+      if (scopeSelect.value === 'global') {
         typeSelect.value = '0';
         typeSelect.disabled = true;
-      }else{
+        globalOption.style.display = "none";
+      } else {
         typeSelect.disabled = false;
-				if (typeSelect.value === '0'){
-          typeSelect.value = ''; 
-        }
+        if (scopeSelect.value === 'type'){
+          // typeSelect.value = '';
+          globalOption.style.display = "none";
+        }  
       }
     }
   </script>
